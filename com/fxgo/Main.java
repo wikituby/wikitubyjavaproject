a8075aed66384b94c80fab0687f7f06868d7c2f8
clyton tumwesigye <herdsontisk@gmail.com>
2023-03-15 16:00:19 +0300

    THIS IS FIRST COMITT

--- com/fxgo/Main.java
+++ com/fxgo/Main.java
@@ -6,19 +6,33 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
+        int simpleCal;
+        Scanner scanner = new Scanner(System.in);
 
-        //vat calculator
-        VatCalculator vatonj = new VatCalculator();
-        vatonj.myVat();
+        System.out.println("Your Simple Calculator");
+        System.out.println("1.Calculate VAT\n2.Calculate NSSF\n3.Calculate Loan");
+        simpleCal = Integer.parseInt(scanner.nextLine());
 
-        // nssf calculator
-        NssfCalculator nssfobj = new NssfCalculator();
-        nssfobj.myNssf();
+        if (simpleCal == 1){
+            //vat calculator
+            VatCalculator vatobj = new VatCalculator();
+            vatobj.myVat();
 
-        //Simple Interest Calculator
-        SimpleInterestCalculator siobj = new SimpleInterestCalculator();
-        siobj.mySical();
+        } else if (simpleCal==2) {
+
+            // nssf calculator
+            NssfCalculator nssfobj = new NssfCalculator();
+            nssfobj.myNssf();
+        } else if (simpleCal==3) {
+
+            //Simple Interest Calculator
+            SimpleInterestCalculator siobj = new SimpleInterestCalculator();
+            siobj.mySical();
+        }
+
+
+
+
 
     }
 
